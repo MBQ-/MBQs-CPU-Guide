638bebf4dc826f2243b6d035c051c6b51a5d8dc3
Kyler <mbqsniper@gmail.com>
2013-10-18 01:02:05 -0700

    Whoops

---
+++ src/com/kyler/mbqscpuguide/Adapters/ListViewAdapters/Governors/ConservativePagerAdapter.java
@@ -0,0 +1,49 @@
+package com.kyler.mbqscpuguide.Adapters.ListViewAdapters.Governors;
+
+import com.kyler.mbqscpuguide.LVFragments.Governors.Definitions.BrazilianWaxDefinition;
+import com.kyler.mbqscpuguide.LVFragments.Governors.Definitions.BrazilianWaxUsage;
+import com.kyler.mbqscpuguide.LVFragments.Governors.Definitions.ConservativeDefinition;
+import com.kyler.mbqscpuguide.LVFragments.Governors.Definitions.ConservativeUsage;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class ConservativePagerAdapter extends FragmentPagerAdapter {
+
+	final int PAGE_COUNT = 2;
+	// Tab Titles
+	private String tabtitles[] = new String[] { "Definition", "Usage" };
+	Context context;
+
+	public ConservativePagerAdapter(FragmentManager fm) {
+		super(fm);
+	}
+
+	@Override
+	public int getCount() {
+		return PAGE_COUNT;
+	}
+
+	@Override
+	public Fragment getItem(int position) {
+		switch (position) {
+
+		case 0:
+			ConservativeDefinition cd = new ConservativeDefinition();
+			return cd;		
+			
+		case 1:
+			ConservativeUsage cu = new ConservativeUsage();
+			return cu;				
+
+		}
+		return null;
+	}
+
+	@Override
+	public CharSequence getPageTitle(int position) {
+		return tabtitles[position];
+	}
+}
--- src/com/kyler/mbqscpuguide/GovernorFragments/Conservative.java
+++ src/com/kyler/mbqscpuguide/GovernorFragments/Conservative.java
@@ -1,32 +1,52 @@
 package com.kyler.mbqscpuguide.GovernorFragments;
 
-import com.kyler.mbqscpuguide.R;
-
-import android.app.Activity;
 import android.os.Bundle;
-import android.support.v4.app.NavUtils;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 
-public class Conservative extends Activity {
+import com.kyler.mbqscpuguide.R;
+import com.kyler.mbqscpuguide.Adapters.ListViewAdapters.Governors.ConservativePagerAdapter;
 
+public class Conservative extends FragmentActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		// Get the view from activity_main.xml
+		setContentView(R.layout.conservative);
+
+		// Locate the viewpager in activity_main.xml
+		ViewPager viewPager = (ViewPager) findViewById(R.id.pager2);
+
+		// Set the ViewPagerAdapter into ViewPager
+		viewPager.setAdapter(new ConservativePagerAdapter(getSupportFragmentManager()));
+	}
+	
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
-        getActionBar().setDisplayHomeAsUpEnabled(true);
-        
-        setContentView(R.layout.conservative);
-        
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.navigate, menu);
+        return super.onCreateOptionsMenu(menu);
     }
-    
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
 
-        case android.R.id.home:
-            NavUtils.navigateUpFromSameTask(this);
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }      
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        
+        return super.onPrepareOptionsMenu(menu);
+    }    
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    switch (item.getItemId()) {
+	        // Respond to the action bar's Up/Home button
+	        case R.id.goBack:
+	            finish();
+	            return true;
+	    }
+	    return super.onOptionsItemSelected(item);
+	}	
 }
